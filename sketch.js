537256218402509355831c2d11e97f2c0398c011
은결 <142661679+ivysuni@users.noreply.github.com>
2024-10-30 09:22:38 +0900

    241020_v02

--- sketch.js
+++ sketch.js
@@ -16,8 +16,25 @@ function draw() {
   if(mouseIsPressed === true){
     console.log(mouseX, mouseY);
   }
+  
+  //skin
   fill('#cf9d7a');
   strokeWeight(0);
   triangle(645, 360, 707, 334, 674, 310);
   
+  fill('#f0c5b4');
+  strokeWeight(0);
+  triangle(572, 344, 617, 335, 568, 316);
+  
+  fill('#eac0b0');
+  strokeWeight(0);
+  triangle(609, 274, 617, 335, 568, 316);
+  
+  fill('#d8b9b6');
+  strokeWeight(0);
+  triangle(547, 255, 609, 274, 568, 316);
+  
+  fill('#e7b497');
+  strokeWeight(0);
+  triangle(618, 334, 609, 274, 674, 310);
 }
